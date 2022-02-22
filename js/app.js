fd704b53189fe87a07f62ab0677e3117afea1b88
Deceit-87 <manuelfreund318@gmail.com>
2022-02-22 01:13:51 +0100

    array oggetti

--- js/app.js
+++ js/app.js
@@ -1,43 +1,76 @@
-const items = [
-    'img/01.jpg',
-    'img/02.jpg',
-    'img/03.jpg',
-    'img/04.jpg',
-    'img/05.jpg'
-];
 
-const title = [
-    'Svezia',
-    'Svizzera',
-    'Gran-Bretagna',
-    'Germania',
-    'Paradise'
+
+const SLIDES =[
+    {
+        imgUrl: 'img/01.jpg' , 
+        titleImg:'Svezia',
+        descriptionImg:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+
+
+    },
+    {
+        imgUrl: 'img/02.jpg' , 
+        titleImg:'SvizzerA',
+        descriptionImg:'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+
+
+    },
+    {
+        imgUrl: 'img/03.jpg' , 
+        titleImg:'Gran-bretagna',
+        descriptionImg:'Lorem ipsum.',
+
+
+    },
+    {
+        imgUrl: 'img/04.jpg' , 
+        titleImg:'Germania',
+        descriptionImg:'Lorem ipsum,cumque provident totam omnis.',
+
+
+    },
+    {
+        imgUrl: 'img/05.jpg' , 
+        titleImg:'Pardise',
+        descriptionImg:'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+
+
+    },
 ]
 
-const text = [
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
-    'Lorem ipsum',
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-    'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-]
+
+SLIDES.forEach((el, i ,array) => {
+console.log(el,i,array)
+});
 
 
 
 
-const containerDivElement = document.getElementsByClassName("container")[0];
 
 
-for (let i = 0; i < items.length; i++) {
-    const divElement = document.createElement('div') ;
-    const imgElement = document.createElement('img')
-    const divTitle = `<h3>${title[i]}</h3><p> ${text[i]} </p> `
+
+
+
+
+
+
+
+
+
+
+// const containerDivElement = document.getElementsByClassName("container")[0];
+
+
+// for (let i = 0; i < items.length; i++) {
+//     const divElement = document.createElement('div') ;
+//     const imgElement = document.createElement('img')
+//     const divTitle = `<h3>${title[i]}</h3><p> ${text[i]} </p> `
   
-    imgElement.src = items[i];
+//     imgElement.src = items[i];
     
-    containerDivElement.append(divElement);
-    divElement.innerHTML = divTitle;
-    divElement.append(imgElement) 
-    divElement.classList.add(title[i])
+//     containerDivElement.append(divElement);
+//     divElement.innerHTML = divTitle;
+//     divElement.append(imgElement) 
+//     divElement.classList.add(title[i])
     
-}
+// }
